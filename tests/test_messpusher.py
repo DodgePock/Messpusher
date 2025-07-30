0f31ed370fca85b892982165a7bf3100821c293d
DodgePock <dodgepock@gmail.com>
2025-07-30 22:38:20 +0800

    rename: ms->mp

--- tests/test_messpusher.py
+++ tests/test_messpusher.py
@@ -1,9 +1,9 @@
 from messpusher import Messpusher
 from config_template import messpusher_config
 
-ms = Messpusher(messpusher_config)
+mp = Messpusher(messpusher_config)
 
 subject_title = 'Test Message'
 subject_message = 'This is a test message from Messpusher.'
 
-ms.send_all(subject_title, subject_message)
+mp.send_all(subject_title, subject_message)
